80eefaf8717e041f0bc750dc6e76eee94e5f754d
Olivier Lemoigne <olivier.lemoigne@ouest-france.fr>
2020-03-03 11:22:10 +0100

    gcd

---
+++ gcd/gcd.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('../getdivisors')
+
+from getdivisors import getdivisors
+
+def gcd(a,b):
+    """
+    return the greatest common divisor
+    """
+    divisors_a = getdivisors(a)
+    divisors_b = getdivisors(b)
+    max = 1
+    for divisor_a in divisors_a:
+        if divisor_a in divisors_b and divisor_a>max:
+            max = divisor_a
+    return max
+
+def euclide(a, b):
+    """
+    return the greatest common divisor with the euclidian method
+    """
+    if b == 1:
+        return 1
+    rest = a % b
+    if rest == 0:
+        return b
+    else:
+        return euclide(b,rest)
---
+++ gcd/gcd_demo.py
@@ -0,0 +1,16 @@
+from gcd import gcd, euclide
+
+a = 64
+b = 24
+print(f"the greatest common divisor of {a} and {b} is {gcd(a,b)}")
+print(f"the greatest common divisor of {a} and {b} is {euclide(a,b)}")
+
+a = 758
+b = 306
+print(f"the greatest common divisor of {a} and {b} is {gcd(a,b)}")
+print(f"the greatest common divisor of {a} and {b} is {euclide(a,b)}")
+
+a = 96
+b = 36
+print(f"the greatest common divisor of {a} and {b} is {gcd(a,b)}")
+print(f"the greatest common divisor of {a} and {b} is {euclide(a,b)}")
